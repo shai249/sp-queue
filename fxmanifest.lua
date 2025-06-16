4c33689a34a9ffcefc2e36c06414150ead67fd6a
Sh1k0P1k0 <59276331+shai249@users.noreply.github.com>
2025-06-16 20:43:55 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,13 +10,9 @@ server_scripts {
     'src/models/Queue.lua', 'src/services/DiscordService.lua',
     'src/services/WebhookService.lua', 'src/services/ValidationService.lua',
     'src/views/CardView.lua', 'src/controllers/QueueController.lua',
-    'src/controllers/CommandController.lua', 'src/QueueManager.lua',
-    'src/utils/TestCommands.lua'
+    'src/controllers/CommandController.lua', 'src/QueueManager.lua'
 }
 
 files {'presentCard.json'}
 
 lua54 'yes'
-
-dependency 'yarn'
-dependency 'webpack'
